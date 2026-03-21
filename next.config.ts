acf82bdbbc484b75bd66e9b64e29cc5fe927d90c
Antony Lin <antonylin0428@gmail.com>
2026-03-21 18:51:52 -0400

    debugged config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["172.25.157.21"],
+};
 
 export default nextConfig;
